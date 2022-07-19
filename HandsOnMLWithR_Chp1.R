2c683c6d82b1ad40a052e82dd968624daf2ee82d
nstamboglis <niccolo.stamboglis@hotmail.com>
2022-07-19 08:36:24 +0100

    Practice code for hands-on ML with R

---
+++ HandsOnMLWithR_Chp1.R
@@ -0,0 +1,7 @@
+# Hand-on machine learning with R
+# Chp 1. Introduction
+# July 2022
+
+# Description: this script provide the R code to practice with the examples provided in the "Hands-on machine learning with R" book.
+# The book can be found here: https://bradleyboehmke.github.io/HOML/intro.html#data
+
