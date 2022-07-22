4b287ff523446939364eddc30568e1b8fcff5818
nstamboglis <niccolo.stamboglis@hotmail.com>
2022-07-22 09:44:15 +0100

    M with R chapt 3 script

--- HandsOnMLWithR_Chp3.R
+++ HandsOnMLWithR_Chp3.R
@@ -114,3 +114,46 @@ ames_recipe %>%
 # imputated based on trees
 ames_recipe %>%
   recipes::step_impute_bag(all_predictors())
+
+# Checking for near-zero variance features (these features are most likely not informative from a rule-of-thumb approach)
+caret::nearZeroVar(ames_train, saveMetrics = TRUE) %>% 
+  tibble::rownames_to_column() %>% 
+  filter(nzv)
+# Rule of thumb
+# 1. The fraction of unique values over the sample size is low (say ≤ 10%);
+# 2- The ratio of the frequency of the most prevalent value to the frequency of the second most prevalent value is large (say ≥ 20%).
+
+# 3.5 variables normalization
+# Normalize all numeric columns (applies the YeoJohnson norm to all numeric variables)
+recipe(Sale_Price ~ ., data = ames_train) %>%
+  step_YeoJohnson(recipes::all_numeric())         
+
+# normalization (recipe for both training and test on the same scale)
+ames_recipe %>%
+  step_center(all_numeric(), -all_outcomes()) %>%
+  step_scale(all_numeric(), -all_outcomes())
+
+# 3.6 Categorical feature engineering
+
+# Lumping (group categoricals with few observations)
+dplyr::count(ames_train, Neighborhood) %>% dplyr::arrange(n)
+count(ames_train, Screen_Porch) %>% arrange(n) # disperse values
+
+# Lump levels for two features (lumping into an "other" category)
+lumping <- recipe(Sale_Price ~ ., data = ames_train) %>%
+  recipes::step_other(Neighborhood, threshold = 0.01, 
+             other = "other") %>%
+  recipes::step_other(Screen_Porch, threshold = 0.1, 
+             other = ">0")
+
+# Apply this blue print --> you will learn about this at 
+# the end of the chapter
+apply_2_training <- recipes::prep(lumping, training = ames_train) %>%
+  recipes::bake(ames_train)
+
+# New distribution of Neighborhood (new and cleaned training data)
+count(apply_2_training, Neighborhood) %>% arrange(n)
+
+# New distribution of Screen_Porch
+count(apply_2_training, Screen_Porch) %>% arrange(n)
+
